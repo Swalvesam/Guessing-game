185fecd3bde51c6cc13d0d2157f96f2969315fec
Samantha Swalve <samantha.swalve@gmail.com>
2021-04-27 20:32:03 +0000

    Generated random number

--- game.py
+++ game.py
@@ -1,8 +1,14 @@
 """A number-guessing game."""
+import random
 
 # Put your code here
 print('Howdy, Whats your name?')
 
 #input the name and save it to a variable
 name = input("type in your name:")
-print(name)
+
+
+# we need to generate a random number between 1 and 100
+
+number = random.randint(1,100)
+
