62398f3065423900f912e377e2b9e3cea5563376
Samantha Swalve <samantha.swalve@gmail.com>
2021-04-28 22:00:21 +0000

    random number not working, trying to save to see if it helps

--- game.py
+++ game.py
@@ -1,39 +1,46 @@
 """A number-guessing game."""
-import random
+from random import randint
 
 # Put your code here
 print('Howdy, Whats your name?')
 
 #input the name and save it to a variable
 name = input("type in your name:")
-
-print("I am thinking of a number between 1 and 100")
 # we need to generate a random number between 1 and 100
-
-number = random.randint(1,100)
-
-count = 0
-# loop untill the number is equal to the guess
-while True:
-# prompt the player to guess the number
-    guess = input("Guess the number  ")
-    try: 
-        int(guess)
+print("I am thinking of a number between 1 and 100")
+number = randint(1,100)
     
-    if int(guess) < 1 or int(guess) > 100:
-        print("Please only pick a number between 1 and 100")
-        count+= 1
-    elif int(guess) < number: 
-        print("Your guess is too low, try again.")
-        count += 1
-    elif int(guess) < number:
-        print("Your guess is too high, try again.")
-        count += 1
-    else:
-        print("You are correct!")
-        print(f"You guessed in {count} tries.")
-        break
+count = 0
+
+    #loop untill the number is equal to the guess
+while True:
+    # prompt the player to guess the number
+    guess = input("Guess the number: ")
+    try: 
+        guess = int(guess)
+            
+        if int(guess) < 1 or int(guess) > 100:
+            print("Please only pick a number between 1 and 100")
+            count+= 1
+        elif int(guess) < number: 
+            print("Your guess is too low, try again.")
+            count += 1
+        elif int(guess) < number:
+            print("Your guess is too high, try again.")
+            count += 1
+        else:
+            print("You are correct!")
+            print(f"You guessed in {count} tries.")
+            break
     except:
         print("please only enter an integer")
         count += 1
+           
+      
+
+
+               
+
+
+
 
